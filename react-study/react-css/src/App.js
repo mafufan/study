32cccee5ecb1178956fe77c825f20b0c7e91d022
mafufan <dhgmlwo98@gmail.com>
2022-07-11 15:50:12 +0900

    react ch09

---
+++ react-study/react-css/src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import SassComTest from './SassComTest';
+
+function App() {
+  return <SassComTest></SassComTest>;
+}
+
+export default App;
---
+++ react-study/react-css/src/SassComTest.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './SassComTest.scss';
+
+const SassComTest = () => {
+  return (
+    <div className="SassComTest">
+      <div className="box red"></div>
+      <div className="box orange"></div>
+      <div className="box yellow"></div>
+      <div className="box green"></div>
+      <div className="box blue"></div>
+      <div className="box indigo"></div>
+      <div className="box violet"></div>
+    </div>
+  );
+};
+
+export default SassComTest;
